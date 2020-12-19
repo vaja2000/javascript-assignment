a0de48e0d527d156de946060db4b2887b85c0570
vaja urmishvili <vazhaurmishvili@gmail.com>
2020-12-19 19:24:38 +0400

    javascript assignment

---
+++ app.js
@@ -0,0 +1,32 @@
+//1. რომელი ციფრი უფრო დიდია?
+
+function greatNum(num1,num2) {
+    if(num1 > num2){
+        console.log("20")
+    }
+    else{
+        console.log("40")
+    }
+ 
+}
+greatNum(20,40)
+
+//2. თარგმნა
+
+function change(lang) {
+    if(lang=="ka")
+    {
+        return "გამარჯობა, მსოფლიო"
+    }
+    if(lang=="en")
+    {
+        return "Hello, world"
+    }
+    return "Hello, world"
+}
+console.log(change("ka"));
+console.log(change("en"));
+console.log(change());
+
+//3. შეფასების სისტემა
+
